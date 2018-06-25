d528de05ee9164a6caddb9e9dc0632f32a00e538
Sean Spade <skspade@protonmail.com>
2018-06-25 08:51:27 -0400

    Created ProcessFrame Method

--- EmguProject/Program.cs
+++ EmguProject/Program.cs
@@ -5,15 +5,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Emgu.CV;
+using Emgu.CV.CvEnum;
 using Emgu.CV.Util;
+using System.Drawing;
 
 namespace EmguProject
 {
     class Program
     {
+        #region Constants
         private const string BackgroundFrameWindowName = "Background Frame";
 
+        private const int Threshold = 5; //Determines boundary of brightness while turning grayscale image to binary (black-white) image
+
+        private const int ErodeIterations = 3; //Erosion to remove noise (reduce white pixel zones)
+        #endregion
+
+        #region Mats
         private static Mat backgroundFrame = new Mat(); // Frame used as a base for change detection 
+
+        private static Mat rawFrame = new Mat(); // Frame as is obtained from video
+        private static Mat diffFrame = new Mat(); //Image showing differences between background and raw frame
+
+        private static Mat grayscaleDiffFrame= new Mat(); //Image showing differences in 8-bit color depth
+        private static Mat binaryDiffFrame = new Mat(); //Image showing changes areas in white and unchanges in black
+
+        private static Mat denoisedDiffFrame = new Mat(); // Image with irrelevant changes removed with opening operation
+
+        private static Mat finalFrame = new Mat(); //Video frame with detected object marked
+
+        #endregion
+
 
         static void Main(string[] args)
         {
@@ -74,5 +96,26 @@ namespace EmguProject
                 }
             }
         }
+
+        private static void ProcessFrame(Mat backgroundFrame, int threshold, int erodeIterations, int dilateIterations)
+        {
+            //Find difference between background (first) frame and current frame
+            CvInvoke.AbsDiff(backgroundFrame, rawFrame, diffFrame);
+
+            //Apply binary threshold to grayscale image (white pixel will mark difference)
+            CvInvoke.CvtColor(diffFrame, grayscaleDiffFrame, ColorConversion.Bgr2Gray);
+            CvInvoke.Threshold(grayscaleDiffFrame, binaryDiffFrame, threshold, 255, ThresholdType.Binary);
+
+
+            //Remove noise with opening operation (erosion followed by dilation)
+            CvInvoke.Erode(binaryDiffFrame, denoisedDiffFrame, null, new Point(-1, -1), erodeIterations,
+                            BorderType.Default, new Emgu.CV.Structure.MCvScalar(1));
+            CvInvoke.Dilate(denoisedDiffFrame, denoisedDiffFrame, null, new Point(-1, -1), dilateIterations,
+                            BorderType.Default, new Emgu.CV.Structure.MCvScalar(1));
+
+            rawFrame.CopyTo(finalFrame);
+            DetectObject(denoisedDiffFrame, finalFrame);
+        }
+        
     }
 }
